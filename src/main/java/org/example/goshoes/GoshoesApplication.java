13c2e6a8f671af140ceb941a59cf9d86f1be3038
datnt11122004 <datnt11122004@gmail.com>
2024-10-17 23:30:55 +0700

    spring boot go shoes

---
+++ src/main/java/org/example/goshoes/GoshoesApplication.java
@@ -0,0 +1,13 @@
+package org.example.goshoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoshoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoshoesApplication.class, args);
+    }
+
+}
